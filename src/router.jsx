649bd6a3ea3ed3fb516203941f3c9a664bb3e597
ManI <letz0703@gmail.com>
2023-10-24 04:54:58 +0900

    ...

--- src/router.jsx
+++ src/router.jsx
@@ -8,11 +8,11 @@ export const router = createBrowserRouter([
     path: "/",
     element: <RootLayout />,
     children: [
-      {index: true, element: <Home />},
-      {
-        path: "/0909",
-        element: "wizbox.shop"
-      }
+      {index: true, element: <Home />}
+      //{
+      //  path: "/0909",
+      //  element: "wizbox.shop"
+      //}
       //{path: "0909", element: window.open("https://wizbox.shop", "_blank")}
     ]
   }
