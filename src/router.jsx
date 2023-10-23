c77faf277ccc386c56459beffb49e2ece5cc5192
ManI <letz0703@gmail.com>
2023-10-24 03:55:21 +0900

    window.open

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,4 @@
-import {Navigate, createBrowserRouter} from "react-router-dom"
+import {Link, Navigate, createBrowserRouter} from "react-router-dom"
 import {RootLayout} from "./layouts/RootLayout"
 import {Home} from "./pages/Home"
 //import {Redirect} from "./pages/Redirect"
@@ -9,7 +9,7 @@ export const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {index: true, element: <Home />},
-      {path: "/0909", element: <Navigate to="wizbox.shop" />}
+      {path: "/0909", element: window.open("https://wizbox.shop", "_blank")}
     ]
   }
 ])
